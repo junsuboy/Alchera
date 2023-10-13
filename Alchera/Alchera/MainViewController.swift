798dbaab7611a2741d7c7c169be59ba78d11758c
junsuboy <junsboyboy@hanyang.ac.kr>
2023-10-13 15:51:56 +0900

    Added SplashView with Lottie

---
+++ Alchera/Alchera/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Alchera
+//
+//  Created by ts3 on 2023/10/13.
+//
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    let animationView = LottieAnimationView(name: "alchera")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        animationView.frame = CGRect(x: 0, y: 0, width: 600, height: 600)
+        animationView.center = view.center
+        animationView.contentMode = .scaleAspectFill
+        
+        view.addSubview(animationView)
+        
+        animationView.play { finish in
+            self.animationView.removeFromSuperview()
+        }
+    }
+}
